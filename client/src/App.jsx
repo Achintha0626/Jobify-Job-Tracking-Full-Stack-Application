31a2e07a612867507f354a57c1c9b1444dd48fa9
Achintha0626 <124795790+Achintha0626@users.noreply.github.com>
2024-12-03 21:34:40 +0530

    patrice React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>home</h1>,
+  },
+  {
+    path: "/about",
+    element: (
+      <div>
+        <h1>about page</h1>
+      </div>
+    ),
+  },
+]);
 
 const App = () => {
-  return (
-    <h1>Jobify</h1>
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
