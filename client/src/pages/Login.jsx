b1e08573c133109e8c6348b2a6f34b56c5c89437
Achintha0626 <124795790+Achintha0626@users.noreply.github.com>
2024-12-04 19:09:08 +0530

    create login page UI

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { Logo } from '../components';
+import FormRow from '../components/FormRow';
 
 const Login = () => {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to='/register'>Register Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>login</h4>
+        <FormRow type="email" name="email" />
+        <FormRow type="password" name="password" />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <button type="button" className="btn btn-block">
+          explore the app
+        </button>
+        <p>
+         Not a member yet?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 }
 
