d215c50630ff37c419f8f1ada916be74849ad31c
Achintha0626 <124795790+Achintha0626@users.noreply.github.com>
2024-12-06 11:11:57 +0530

    create dashboard strucher

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/Dashboard'
+import { BigSidebar, Navbar, SmallSidebar } from '../components'
 
 const DashboardLayout = () => {
   return (
-   <div>
+   <Wrapper>
+    <main className="dashboard">
+
+      <SmallSidebar/>
+      <BigSidebar/>
+      <div>
+        <Navbar/>
+        <div className='dashboard-page'>
+
+        </div>
+      </div>
+
+    </main>
     <Outlet/>
-   </div>
+   </Wrapper>
   )
 }
 
