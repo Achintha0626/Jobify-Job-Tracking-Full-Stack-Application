416b194d0dc69eb87914f41998d4596f8ae1e49c
Achintha0626 <124795790+Achintha0626@users.noreply.github.com>
2024-12-06 19:17:09 +0530

    create new componet call NavLink and add the navigation links to that component

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -5,6 +5,7 @@ import { FaTimes } from 'react-icons/fa';
 import Logo from './Logo';
 import links from '../utils/links';
 import { NavLink } from 'react-router-dom';
+import NavLinks from './NavLinks';
 
 const SmallSidebar = () => {
 
@@ -20,18 +21,9 @@ const SmallSidebar = () => {
           <header>
             <Logo/>
           </header>
-          <div className="nav-links">
-            {links.map((link)=>{
-              const {text,path,icon}=link
-              return <NavLink to={path} key={text} className='nav-link' onClick={toggleSidebar}>
-                <span className='icon'>
-                  {icon}
-                </span>
-                {text}
-              </NavLink>
-              
-            })}
-          </div>
+
+          <NavLinks/>
+       
         </div>
       </div>
     </Wrapper>
