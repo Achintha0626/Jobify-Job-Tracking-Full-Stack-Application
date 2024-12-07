8b118f26a6859d644b003922de7291c964f09d9f
Achintha0626 <124795790+Achintha0626@users.noreply.github.com>
2024-12-08 00:05:42 +0530

    create the database connection
    packages are not necessary

--- jobify/server.js
+++ jobify/server.js
@@ -2,23 +2,17 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import jobRouter from "./routes/jobRouter.js";
 
 const app = express();
-
-import morgan from "morgan";
-
-import jobRouter from './routes/jobRouter.js'
-
-
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-
-
 app.use(morgan("dev"));
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -27,27 +21,29 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   console.log(req);
-  res.json({ message: "data recived", data: req.body });
+  res.json({ message: "data received", data: req.body });
 });
 
+app.use("/api/v1/jobs", jobRouter);
 
-app.use('/api/v1/jobs',jobRouter)
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
 
-
-app.use('*',(req,res)=>{
-  //This middleware catches any requests that don't match any existing routes
-  res.status(404).json({ msg: "not fond" });
-})
-
-app.use((err,req,res,next)=>{
-  //This middleware is for catching errors that happen during request processing.
+app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).json({ msg: "something went wrong" });
-})
-
-
-const port=process.env.PORT || 5100
-
-app.listen(port, () => {
-  console.log(`Server Running on PORT ${port}...`);
 });
+
+const port = process.env.PORT || 5100;
+
+try {
+  // console.log(process.env.MONGO_URL); // Check if it's loaded correctly
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}....`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
