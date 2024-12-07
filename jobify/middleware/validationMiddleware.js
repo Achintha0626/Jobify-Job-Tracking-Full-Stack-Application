d8910b69b7f4f8809481819ec4609b58f8a23192
Achintha0626 <124795790+Achintha0626@users.noreply.github.com>
2024-12-08 03:49:26 +0530

    Validate Job

--- jobify/middleware/validationMiddleware.js
+++ jobify/middleware/validationMiddleware.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 import { param } from "express-validator";
 import { body, validationResult } from "express-validator";
-import { BadRequestError } from "../errors/customErrors.js";
+import { BadRequestError, NotFoundError } from "../errors/customErrors.js";
 import { JOB_STATUS, JOB_TYPE } from "../utils/constants.js";
+import Job from '../models/JobModel.js'
 const withValidationErrors = (validateValues) => {
   return [
     validateValues,
@@ -11,6 +12,9 @@ const withValidationErrors = (validateValues) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         const errorMessages = errors.array().map((error) => error.msg);
+        if(errorMessages[0].startWith('no job')){
+          throw new NotFoundError(errorMessages)
+        }
         throw new BadRequestError(errorMessages);
       }
       next();
@@ -29,7 +33,12 @@ export const validateJobInput = withValidationErrors([
 ]);
 
 export const validateIdParam = withValidationErrors([
-  param("id")
-    .custom((value) => mongoose.Types.ObjectId.isValid(value))
-    .withMessage("invalid MongoDB id"),
+  param("id").custom(async (value) => {
+    const isValidId = mongoose.Types.ObjectId.isValid(value);
+    if (!isValidId) throw new BadRequestError("invalid MongoDB id");
+    const job = await Job.findById(value);
+    if (!job) throw new NotFoundError(`no job with id : ${value}`);
+  }),
 ]);
+
+
