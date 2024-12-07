ebb80b032207457c794326d6b5546158d157933b
Achintha0626 <124795790+Achintha0626@users.noreply.github.com>
2024-12-08 00:24:35 +0530

    create job models and controller

--- jobify/controllers/jobController.js
+++ jobify/controllers/jobController.js
@@ -1,3 +1,5 @@
+import Job from '../models/JobModel.js'
+
 import { nanoid } from "nanoid";
 
 let jobs = [
@@ -9,16 +11,12 @@ export const getAllJobs = async (req, res) => {
   res.status(200).json({ jobs });
 };
 
-export const createJob = async (req, res) => {
-  const { company, position } = req.body;
 
-  if (!company || !position) {
-    return res.status(400).json({ msg: "please provide company and position" });
-  }
-  const id = nanoid(10);
-  const job = { id, company, position };
-  jobs.push(job);
-  res.status(200).json({ job });
+
+export const createJob = async (req, res) => {
+  
+  const job=await Job.create(req.body)
+  res.status(201).json({ job });
 };
 
 export const getJob = async (req, res) => {
