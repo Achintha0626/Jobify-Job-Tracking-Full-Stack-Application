84ce78dae08798af8c85a72e7b76b3c497100318
Achintha0626 <124795790+Achintha0626@users.noreply.github.com>
2024-12-07 22:39:57 +0530

    create controller

--- jobify/server.js
+++ jobify/server.js
@@ -6,13 +6,8 @@ import express from "express";
 const app = express();
 
 import morgan from "morgan";
-import { nanoid } from "nanoid";
 
-let jobs = [
-  { id: nanoid(), company: "apple", position: "front-end" }, //Why Use nanoid() for IDs? To uniquely identify each job even if multiple jobs have the same company or position.
-  { id: nanoid(), company: "google", position: "back-end" },
-];
-
+import jobRouter from './routes/jobRouter.js'
 
 
 
@@ -36,66 +31,7 @@ app.post("/", (req, res) => {
 });
 
 
-//Get All jobs
-app.get('/api/v1/jobs',(req,res)=>{
-  
-  res.status(200).json({jobs})
-})
-
-//create job
-app.post("/api/v1/jobs", (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-     res.status(400).json({ msg: "please provide company and position" });
-  }
-  const id = nanoid(10);
-  // console.log(id);
-  const job = { id, company, position };
-  jobs.push(job);
-  res.status(201).json({ job });
-});
-
-
-//Get Single Job
-app.get("/api/v1/jobs/:id", (req, res) => {
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
-  res.status(200).json({ job });
-});
-
-//Edit Job
-app.patch("/api/v1/jobs/:id", (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: "please provide company and position" });
-  }
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
-
-  job.company = company;
-  job.position = position;
-  res.status(200).json({ msg: "job modified", job });
-});
-
-// DELETE JOB
-
-app.delete('/api/v1/jobs/:id', (req, res) => {
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
-  const newJobs = jobs.filter((job) => job.id !== id);
-  jobs = newJobs;
-
-  res.status(200).json({ msg: 'job deleted' });
-});
+app.use('/api/v1/jobs',jobRouter)
 
 
 app.use('*',(req,res)=>{
