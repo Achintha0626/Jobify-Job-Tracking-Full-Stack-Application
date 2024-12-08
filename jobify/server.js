501b6116fdf23d2351dd31e44e5830a6e9678df3
Achintha0626 <124795790+Achintha0626@users.noreply.github.com>
2024-12-08 23:44:33 +0530

    proxy setup

--- jobify/server.js
+++ jobify/server.js
@@ -33,6 +33,10 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/api/v1/test", (req, res) => {
+  res.json({ msg: "test route" });
+});
+
 
 
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
