de42dcec3e9e816c30b0e6d51c264d80a045f9ce
Achintha0626 <124795790+Achintha0626@users.noreply.github.com>
2024-12-09 11:09:04 +0530

    create a function to login using database

--- jobify/client/src/pages/Login.jsx
+++ jobify/client/src/pages/Login.jsx
@@ -1,32 +1,49 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, Form, redirect, useNavigation } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
-import { Logo } from '../components';
+import {  Logo } from "../components";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+
 import FormRow from '../components/FormRow';
 
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  try {
+    await customFetch.post("/auth/login", data);
+    toast.success("Login successful");
+    return redirect("/dashboard");
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+    return error;
+  }
+};
+
 const Login = () => {
+  const navigation=useNavigation()
+  const isSubmitting=navigation.state==='submitting'
   return (
     <Wrapper>
-      <form className="form">
+      <Form method="post" className="form">
         <Logo />
         <h4>login</h4>
         <FormRow type="email" name="email" />
         <FormRow type="password" name="password" />
-        <button type="submit" className="btn btn-block">
-          Submit
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting?'submitting':'submit'}
         </button>
         <button type="button" className="btn btn-block">
           explore the app
         </button>
         <p>
-         Not a member yet?
+          Not a member yet?
           <Link to="/register" className="member-btn">
             Register
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
-}
+};
 
-export default Login
+export default Login;
