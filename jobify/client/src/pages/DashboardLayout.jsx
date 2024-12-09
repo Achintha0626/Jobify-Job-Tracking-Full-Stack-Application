c2c1b91fcde1b4f7ca34502e3d88bf50a2c23965
Achintha0626 <124795790+Achintha0626@users.noreply.github.com>
2024-12-09 12:53:31 +0530

    implement logout feature

--- jobify/client/src/pages/DashboardLayout.jsx
+++ jobify/client/src/pages/DashboardLayout.jsx
@@ -1,9 +1,10 @@
 import React, { createContext, useContext, useState } from "react";
-import { Outlet, redirect, useLoaderData } from "react-router-dom";
+import { Outlet, redirect, useLoaderData, useNavigate } from "react-router-dom";
 import Wrapper from "../assets/wrappers/Dashboard";
 import { BigSidebar, Navbar, SmallSidebar } from "../components";
 import { checkDefaultTheme } from "../App";
 import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
 
 export const loader= async()=>{
   try {
@@ -22,7 +23,7 @@ const DashboardContext = createContext();
 
 const DashboardLayout = ({ isDarkThemeEnabled }) => {
   const {user} = useLoaderData();
-  
+  const navigate= useNavigate()
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(isDarkThemeEnabled);
 
@@ -37,8 +38,11 @@ const DashboardLayout = ({ isDarkThemeEnabled }) => {
     setShowSidebar(!showSidebar);
   };
 
-  const loggoutUser = async () => {
-    console.log("logout user");
+  const logoutUser = async () => {
+     await customFetch.get("auth/logout");
+    navigate('/')
+    toast.success('Logging out..')
+    
   };
 
   return (
@@ -50,7 +54,7 @@ const DashboardLayout = ({ isDarkThemeEnabled }) => {
           isDarkTheme,
           toggleDarkTheme,
           toggleSidebar,
-          loggoutUser,
+          logoutUser,
         }}
       >
         <main className="dashboard">
