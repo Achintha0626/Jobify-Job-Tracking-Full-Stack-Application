212a7a53267b18b31b080099dfd980d0c08babbb
Achintha0626 <124795790+Achintha0626@users.noreply.github.com>
2024-12-11 19:33:10 +0530

    create a delay when the user enter some key word
    [JS Nuggets - Debounce](https://youtu.be/tYx6pXdvt1s)
    
    In JavaScript, debounce is a way to limit how often a function gets called. It helps prevent rapid or repeated function executions by introducing a delay. This is useful for tasks like handling user input, where you want to wait for a pause before triggering an action to avoid unnecessary processing.

--- jobify/client/src/components/SearchContainer.jsx
+++ jobify/client/src/components/SearchContainer.jsx
@@ -10,6 +10,17 @@ const SearchContainer = () => {
   const {searchValues}=useAllJobsContext()
   const {search, jobStatus, jobType, sort}=searchValues
   const submit=useSubmit()
+
+  const debounce = (onChange) => {
+    let timeout;
+    return (e) => {
+      const form = e.currentTarget.form;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        onChange(form);
+      }, 1000);
+    };
+  };
  
   return (
     <Wrapper>
@@ -17,15 +28,15 @@ const SearchContainer = () => {
         <h5 className="form-title">search form</h5>
         <div className="form-center">
           {/* search position */}
-
           <FormRow
             type="search"
             name="search"
             defaultValue={search}
-            onChange={(e) => {
-              submit(e.currentTarget.form);
-            }}
+            onChange={debounce((form) => {
+              submit(form);
+            })}
           />
+          
           <FormRowSelect
             labelText="job status"
             name="jobStatus"
@@ -52,7 +63,6 @@ const SearchContainer = () => {
               submit(e.currentTarget.form);
             }}
           />
-
           <Link to="/dashboard/all-jobs" className="btn form-btn delete-btn">
             Reset Search Values
           </Link>
